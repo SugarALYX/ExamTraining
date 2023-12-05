8c718e4178298d1e80901ddabd66e52a88c9e512
SugarSingularity <79350028+SugarSingularity@users.noreply.github.com>
2023-12-05 17:30:09 +0300

    Улучшения и прочее...

--- Подготовка к ЕГЭ/Создание Задания с парсингом.py
+++ Подготовка к ЕГЭ/Создание Задания с парсингом.py
@@ -7,6 +7,16 @@ tprint("AUTHOR: ALYX")
 N = int(input("Сколько задач хотите решить:   "))
 
 
+def Create_description(Fp: str, Problem: str) -> "description file":
+    """
+    :param Fp: Путь к папке
+    :param Problem: Описание задания
+    :return: Файл формата md
+    """
+    with open(Fp, "w", encoding="utf-8") as f:
+        f.write(Problem)
+
+
 def CreatbyPrefers():
     """
     Создание файлов формато py и md + парсинг и запись данных
@@ -14,7 +24,7 @@ def CreatbyPrefers():
     :return:
     """
     TN = input("Введите номер задания:  ")
-    url = input("Введите ссылку на задание:  ")
+    url = input("Введите ссылку на определённое задание из сайта РЕШУ ЕГЭ:  ")
 
     responce = requests.get(url)  # парсинг
     soup = BeautifulSoup(responce.text, "lxml")
@@ -37,9 +47,11 @@ def CreatbyPrefers():
     Filepath = os.path.join(FolderPath, f"{NewFolder}.py")
     with open(Filepath, "w") as f:  # Создание файла формата py в этой папке
         f.write(f"")
-    Filepath = os.path.join(FolderPath, f"{NewFolder}.md") # Создание файла формата md в этой папке
-    with open(Filepath, "w", encoding="utf-8") as f:
-        f.write(issue)
+    Filepath = os.path.join(FolderPath, f"{NewFolder}.md")  # Создание файла формата md в этой папке
+    print("Описание задание? \n 1. Нужно \n 2. Ненужно")
+    choose2 = int(input("->  "))
+    if choose2 == 1:
+        Create_description(Filepath, issue)
 
 
 def CreatbyRandom():
@@ -48,13 +60,15 @@ def CreatbyRandom():
 
 while True:
     try:
-        print("Выберите способ создания:  \n 1.Простой (Вы сами вводите желаемое задание) \n 2.Рандомный")
-        choose = int(input("->  "))
-        if choose == 1:
+        print(
+            "Выберите способ создания:  \n 1.Простой (Вы сами вводите желаемое задание) \n 2.Рандомный(ещё не "
+            "доработан...)")
+        choose1 = int(input("->  "))
+        if choose1 == 1:
             for i in range(N):
                 CreatbyPrefers()
             break
-        elif choose == 2:
+        elif choose1 == 2:
             CreatbyRandom()
             break
     except ValueError:
